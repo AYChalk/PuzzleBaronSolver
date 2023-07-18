2193fe9c6a4061a681dea8bbcb70869f0453a00b
AYChalk <yitzchokchalk@gmail.com>
2023-07-18 22:02:48 +0100

    StarBattle tests

---
+++ src/Test/StarBattleTests/StarBattleSolverTest.java
@@ -0,0 +1,18 @@
+package StarBattleTests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StarBattleSolverTest {
+
+    @org.junit.jupiter.api.Test
+    void setStarBattlePuzzle() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void solve() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void incrementCurrentStars() {
+    }
+}
